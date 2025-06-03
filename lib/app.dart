30e401ae51527b9417a5223958fd6b167a8c84d2
Pedro Henrique R. Menezes <pedrohrm050315@hotmail.com>
2025-06-03 05:20:09 -0300

    API and tables

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
-// import 'dart:convert';
-// import 'component/book.dart';
-// import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 class App extends StatefulWidget {
   @override
@@ -9,26 +8,45 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  // List<Book> _books = [];
+  List<TableRow> tables = [];
+  List<List<Widget>> _rows = [];
+
   @override
   void initState() {
     super.initState();
-    // _fetchBooks();
+    _fillTable();
+
   }
 
-  // Future<void> _fetchBooks() async {
-  //   final response = await http.get(
-  //     Uri(scheme: 'http', host: '127.0.0.1', path: '/', port: 3000),
-  //   );
-  //   if (response.statusCode == 200) {
-  //     final List<dynamic> json = jsonDecode(response.body);
-  //     setState(() {
-  //       _books = json.map((item) => Book.fromJson(item)).toList();
-  //     });
-  //   } else {
-  //     throw Exception('Failed to load books');
-  //   }
-  // }
+  Future<void> _fillTable() async {
+    final response = await http.get(Uri.parse('http://192.168.18.11:3000/api'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> decodeJson = jsonDecode(response.body);
+      final Iterable<dynamic> ids = decodeJson
+          .map((e) => e.remove('id'))
+          .toList();
+
+      print(decodeJson);
+
+      if (decodeJson.isNotEmpty) {
+        List<Widget> _columns = [];
+        decodeJson[0].forEach((key, value) => _columns.add(Text(key)));
+
+        _rows.add(_columns);
+        _rows = decodeJson.map((e) {
+          List<Widget> a = [];
+          e.forEach((key, value) => a.add(Text(value.toString())));
+          return a;
+        }).toList();
+        print(_rows);
+
+        _rows.forEach((e) => tables.add(TableRow(children: e)));
+      }
+    } else {
+      throw Exception('Failed to load books');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,27 +60,18 @@ class _AppState extends State<App> {
           2: FixedColumnWidth(64),
         },
         defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-        children: <TableRow>[
-          TableRow(
-            children: <Widget>[
-              Text('col 1'),
-              Text('col 2'),
-              Text('col 3'),
-              Text('col 3'),
-              Text('col 3'),
-            ],
-          ),
-          TableRow(
-            decoration: const BoxDecoration(color: Colors.grey),
-            children: <Widget>[
-              Text('col 1'),
-              Text('col 2'),
-              Text('col 3'),
-              Text('col 4'),
-              Text('col 5'),
-            ],
-          ),
-        ],
+        children: tables
+        // children: <TableRow>[
+        //   TableRow(
+        //     children: <Widget>[
+        //       Text('col 1'),
+        //       Text('col 2'),
+        //       Text('col 3'),
+        //       Text('col 3'),
+        //       Text('col 3'),
+        //     ],
+        //   ),
+        // ],
       ),
       // body: ListView.builder(
       //   itemCount: _books.length,
